4a176ce3291581041569b8ec9e3413b6ac76f823
Gulshan <abhinavg90834@gmail.com>
2025-07-16 18:46:23 +0530

    feat: login and protected route functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,53 @@
 import express from 'express';
+import JWT from 'jsonwebtoken';
 
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
+
+app.use(express.json());
 
 app.get('/api/message', (_req, res) => {
   res.send('Welcome to Event Desk');
 });
 
+app.get('/api/protected', (req, res) => {
+  try {
+    const token = req.headers['authorization'];
+    if (token) {
+      const decoded = JWT.verify(token, 'eventDesk');
+
+      return res.json({
+        message: 'token verified',
+        decoded,
+      });
+    } else {
+      return res.status(400).json({
+        message: 'Bad request',
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: 'server error',
+    });
+  }
+});
+
+app.post('/api/login', (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email === 'admin@gmail.com' && password === 'admin') {
+      const token = JWT.sign({ email }, 'eventDesk', { expiresIn: '1h' });
+      res.status(200).json({ message: 'Login Success', token });
+    } else {
+      res.status(401).json({ message: 'Login Failed' });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: 'server error' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Backend running at http://localhost:${PORT}`);
 });
-
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,72 @@
 import { useState } from 'react';
-import { Button } from 'react-daisyui';
+import { Button, Input } from 'react-daisyui';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-  const fetchMessage = async () => {
-    const res = await fetch('/api/message');
-    const text = await res.text();
-    setMessage(text);
+  const handleLogin = async () => {
+    try {
+      const res = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          password: formData.password,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      localStorage.setItem('authToken', data.token);
+      setMessage(data.message);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const callProcted = async () => {
+    try {
+      const res = await fetch('/api/protected', {
+        method: 'GET',
+        headers: {
+          authorization: localStorage.getItem('authToken') || '',
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+      console.log('protected');
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <Button color="primary" onClick={fetchMessage}>
-        Get Welcome Message
+    <div className='flex flex-col items-center justify-center min-h-screen gap-4'>
+      <Input
+        type='email'
+        placeholder='Email'
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      />
+      <Input
+        type='password'
+        placeholder='Password'
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+      />
+
+      <Button color='primary' onClick={handleLogin}>
+        Login
       </Button>
-      {message && <div className="text-lg font-bold">{message}</div>}
+      <Button color='primary' onClick={callProcted}>
+        Call Procted
+      </Button>
+      {message && <div className='text-lg font-bold'>{message}</div>}
     </div>
   );
 }
 
 export default App;
-
